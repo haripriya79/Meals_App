7ae212ccffd0300ca9e10ae2f1c2568e2ff230c6
Haripriya Gangavaram <gangavaramharipriya@gmail.com>
2021-04-22 11:03:47 +0530

    commit on master

---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:Meals_App/category_item.dart';
+import 'package:Meals_App/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title:const Text('DeliMeals'),
+      ),
+      body: GridView( 
+        padding: EdgeInsets.all(15),       
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(catData.id,catData.title, catData.color),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
